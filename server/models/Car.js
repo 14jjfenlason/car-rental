ff5e6a256c2641eb2f1bd43dd79112cf47e66b5a
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-06-12 11:18:38 -0500

    made changes to model folder

--- server/models/Car.js
+++ server/models/Car.js
@@ -3,27 +3,33 @@ const { Schema } = require("mongoose");
 // THIS WILL NEED TO BE A MODEL
 const carSchema = new Schema({
 
-  carMake: {
+  make: {
     type: String,
     required: true,
   },
-  carModel: {
+
+  model: {
     type: String,
     required: true,
   },
-  // // saved book id from GoogleBooks
-  // carId: {
-  //   type: String,
-  //   required: true,
-  // },
+
+  stock: {
+    type: String,
+    require: true,
+  },
+
+  mileage: {
+    type: String,
+    required: true,
+  },
+
   image: {
     type: String,
     required: true
   },
-  // this allows you to keep track of how many are available
-  // stock:
 
 });
 // make this a model
+const Car = model('Car', carSchema);
 
 module.exports = Car;
